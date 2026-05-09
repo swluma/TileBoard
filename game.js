0a7116574ac9a65e41a308d0d8d97efb97c523db
swluma <wirde6115@gmail.com>
2026-05-09 13:51:15 +0200

    added Territory Battle

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-window.VIBE_GAME_HUB = {
+const VIBE_GAME_HUB_CONFIG = {
   title: "Territory Battle",
   description: "A local turn-based territory battle game where players move, paint tiles, fight over space, use character skills, and compete for final points.",
   icon: "./icon.png",
@@ -6,3 +6,7 @@ window.VIBE_GAME_HUB = {
   supportsLocal: true,
   supportsRooms: false
 };
+
+window.VIBE_GAME_HUB = VIBE_GAME_HUB_CONFIG;
+window.vibegamehub = VIBE_GAME_HUB_CONFIG;
+window.gameConfig = VIBE_GAME_HUB_CONFIG;
